38f7c3b2482929119acb02476703a11076a73bc7
fourndo <fourndo@gmail.com>
2022-12-17 10:55:43 -0800

    Remove commented out code

--- SimPEG/dask/electromagnetics/static/resistivity/simulation.py
+++ SimPEG/dask/electromagnetics/static/resistivity/simulation.py
@@ -194,49 +194,6 @@ def compute_J(self, f=None, Ainv=None):
 Sim.compute_J = compute_J
 
 
-# # This could technically be handled by dask.simulation, but doesn't seem to register
-# @dask.delayed
-# def dask_dpred(self, m=None, f=None, compute_J=False):
-#     """
-#     dpred(m, f=None)
-#     Create the projected data from a model.
-#     The fields, f, (if provided) will be used for the predicted data
-#     instead of recalculating the fields (which may be expensive!).
-#
-#     .. math::
-#
-#         d_\\text{pred} = P(f(m))
-#
-#     Where P is a projection of the fields onto the data space.
-#     """
-#     if self.survey is None:
-#         raise AttributeError(
-#             "The survey has not yet been set and is required to compute "
-#             "data. Please set the survey for the simulation: "
-#             "simulation.survey = survey"
-#         )
-#
-#     if f is None:
-#         if m is None:
-#             m = self.model
-#         f, Ainv = self.fields(m, return_Ainv=compute_J)
-#
-#     data = Data(self.survey)
-#     for src in self.survey.source_list:
-#         for rx in src.receiver_list:
-#             data[src, rx] = rx.eval(src, self.mesh, f)
-#
-#     if compute_J:
-#         self._Jmatrix = None
-#         Jmatrix = self.compute_J(f=f, Ainv=Ainv)
-#         return mkvc(data), Jmatrix
-#
-#     return mkvc(data)
-
-
-
-
-
 def dask_getSourceTerm(self):
     """
     Evaluates the sources, and puts them in matrix form
